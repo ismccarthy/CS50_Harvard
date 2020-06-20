bf66f000250a934fe9411bd41a6b312328b65c38
Ian McCarthy <ianmccarthy.data@gmail.com>
2020-06-21 00:22:29 +1000

    update Saturday night

--- Lecture2_Flask/application.py
+++ Lecture2_Flask/application.py
@@ -1,11 +1,38 @@
-from flask import Flask
+import datetime
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    now = datetime.datetime.now()
+    new_year = now.month == 1 and now.day == 1
+    new_year = True
+    # return render_template('Web001.html', new_year=new_year)
+    return render_template('Web001.html', new_year=new_year)
+
+
+# @app.route("/")
+# def index():
+#     return "Hello World!"
+
+# @app.route("/")
+# def index():
+#     headline = "This is Page One!"
+#     return render_template('Web001.html', headline=headline)
+#
+#
+# @app.route("/today")
+# def today():
+#     now = datetime.datetime.now()
+#     headline = f'Today it is.. {now}'
+#
+#     sunday = 1 == 1
+#
+#     # return render_template('Web001.html', headline=headline)
+#     return render_template('Web001.html', sunday=sunday)
 
 
 @app.route("/nietzsche")
